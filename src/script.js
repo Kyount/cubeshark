7a71889edbf6d05fde17db86567b1934010fac5f
Kyle Yount <kyle.j.yount@gmail.com>
2018-03-16 22:09:48 -0700

    about and contact boxes

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
 var vm = new Vue({
   el: '#app',
   data: {
-
+    aboutBox: false,
+    contactBox: false
   },
   computed: {
 
   },
   methods: {
+    showAbout: function() {
+      this.hideContact();
+      this.aboutBox = true;
+    },
+    hideAbout: function() {
+      this.aboutBox = false;
+    },
+    showContact: function() {
+      this.hideAbout();
+      this.contactBox = true;
+    },
+    hideContact: function() {
+      this.contactBox = false;
+    }
 
+  },
+  mounted: function() {
+
+    document.getElementById('fadeIn').velocity({opacity:0}, 1000).velocity({display:'none'},0);
   }
 });
