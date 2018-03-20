bc63c9ea05ffeeaf1464f2c303230233021be056
Kyle Yount <kyle.j.yount@gmail.com>
2018-03-19 20:16:16 -0700

    Working loading sounds and pausing

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
+var widget;
+
 var vm = new Vue({
   el: '#app',
   data: {
     aboutBox: false,
-    contactBox: false
+    contactBox: false,
+    playing: false,
+    firstStart: false,
   },
   computed: {
 
@@ -21,11 +25,33 @@ var vm = new Vue({
     },
     hideContact: function() {
       this.contactBox = false;
+    },
+    justPlay: function() {
+      this.firstStart = true;
+      document.querySelector('#custompause svg').classList.add('fa-pause');
+      document.querySelector('#custompause svg').classList.remove('fa-play');
+    },
+    togglePlaying: function() {
+      this.firstStart = true;
+      widget.toggle();
+      this.playing = !this.playing;
+      document.querySelector('#custompause svg').classList.toggle('fa-pause');
+      document.querySelector('#custompause svg').classList.toggle('fa-play');
+    },
+    loadTrack: function(track) {
+      document.getElementById('custompause').classList.remove('hidden');
+      widget.bind('SC.Widget.Event[PLAY]', this.justPlay());
+      widget.load(track + "&color=%238ACD94&auto_play=true&inverse=true");
     }
-
   },
   mounted: function() {
-
-    document.getElementById('fadeIn').velocity({opacity:0}, 1000).velocity({display:'none'},0);
+    document.getElementById('fadeIn')
+    .velocity({opacity:0}, {duration: 1000})
+    .velocity({display:'none'}, {duration: 0, complete:
+      function() {
+        document.getElementById("player").style.display = "block";
+         widget = SC.Widget(document.getElementById('iframe'));
+      }
+    });
   }
 });
