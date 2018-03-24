3d7ab4b1d51cded2ed0939323b2d98bf9a94997b
Kyle <kyle.j.yount@gmail.com>
2018-03-23 17:29:39 -0700

    Working description folder system

--- src/script.js
+++ src/script.js
@@ -7,6 +7,13 @@ var vm = new Vue({
     contactBox: false,
     playing: false,
     firstStart: false,
+    tracks: [
+      {showDesc: false, track: 'grace', title: 'Grace', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'mind-song', title: 'Mind Song', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'unused-tv-intro', title: 'Intro Music for Maintitle', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'startup-promo', title: 'Promo Ambiance for TV Spot', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'}
+    ]
   },
   computed: {
 
@@ -38,10 +45,23 @@ var vm = new Vue({
       document.querySelector('#custompause svg').classList.toggle('fa-pause');
       document.querySelector('#custompause svg').classList.toggle('fa-play');
     },
-    loadTrack: function(track) {
+    loadTrack: function(track, index) {
       document.getElementById('custompause').classList.remove('hidden');
       widget.bind('SC.Widget.Event[PLAY]', this.justPlay());
-      widget.load(track + "&color=%2363a088&auto_play=true");
+      widget.load(track + this.tracks[index].track + "&color=%2363a088&auto_play=true");
+    },
+    toggleDescription: function(index, exc) {
+      for (var i=0; i<this.tracks.length; i++) {
+        if (index == i) {
+          if (exc) {
+              this.tracks[i].showDesc = true;
+          } else {
+              this.tracks[i].showDesc = !this.tracks[i].showDesc;
+          }
+        } else {
+          this.tracks[i].showDesc = false;
+        }
+      }
     }
   },
   mounted: function() {
@@ -50,7 +70,7 @@ var vm = new Vue({
     .velocity({display:'none'}, {duration: 0, complete:
       function() {
         document.getElementById("player").style.display = "block";
-         widget = SC.Widget(document.getElementById('iframe'));
+        widget = SC.Widget(document.getElementById('iframe'));
       }
     });
   }
