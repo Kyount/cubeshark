ba4fc7ab322df678886c94198e01afeda7317707
Kyle <kyle.j.yount@gmail.com>
2018-03-23 19:37:07 -0700

    overall big change in styles and functionality

--- src/script.js
+++ src/script.js
@@ -12,11 +12,20 @@ var vm = new Vue({
       {showDesc: false, track: 'mind-song', title: 'Mind Song', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
       {showDesc: false, track: 'unused-tv-intro', title: 'Intro Music for Maintitle', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
       {showDesc: false, track: 'startup-promo', title: 'Promo Ambiance for TV Spot', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
-      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'}
+      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'otto-motors-promo', title: 'Music for Conference Visuals', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'grace', title: 'Grace', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'mind-song', title: 'Mind Song', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'unused-tv-intro', title: 'Intro Music for Maintitle', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
+      {showDesc: false, track: 'startup-promo', title: 'Promo Ambiance for TV Spot', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac malesuada ipsum, eget ullamcorper ex. Quisque scelerisque, dolor vel scelerisque molestie, enim eros finibus nunc, quis porta lorem ipsum sit amet turpis. Morbi aliquam sem a pharetra convallis. Sed commodo varius blandit. Aenean maximus libero ut lacus ultricies bibendum. In fermentum porttitor mattis.'},
     ]
   },
   computed: {
-
+    isMobileDevice: function() {
+      return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    }
   },
   methods: {
     showAbout: function() {
@@ -34,7 +43,9 @@ var vm = new Vue({
       this.contactBox = false;
     },
     justPlay: function() {
+      console.log('justplay');
       this.firstStart = true;
+      widget.play();
       document.querySelector('#custompause svg').classList.add('fa-pause');
       document.querySelector('#custompause svg').classList.remove('fa-play');
     },
@@ -47,8 +58,15 @@ var vm = new Vue({
     },
     loadTrack: function(track, index) {
       document.getElementById('custompause').classList.remove('hidden');
-      widget.bind('SC.Widget.Event[PLAY]', this.justPlay());
-      widget.load(track + this.tracks[index].track + "&color=%2363a088&auto_play=true");
+      widget.bind('SC.Widget.Event["PLAY"]', this.justPlay());
+      //Soundcloud doesn't allow autoplay on mobile... time for desperate measures
+      if (this.isMobileDevice) {
+        console.log('detected mobile');
+        setTimeout(function() {
+          vm.justPlay();
+        }, 3000);
+      }
+      widget.load(track + this.tracks[index].track + "&color=%238acd94&auto_play=true&inverse=false");
     },
     toggleDescription: function(index, exc) {
       for (var i=0; i<this.tracks.length; i++) {
